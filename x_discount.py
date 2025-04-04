c33535c2f9206b18938420246c61b47e6404e95e
202510798kgjk <202510798kgjk@gmail.com>
2025-04-04 15:54:36 +0900

    Delete x_discount.py

--- x_discount.py
+++
@@ -1,5 +0,0 @@
-def fixed_price(price,discount_rate=100):
-    if price <=0:
-        print("price value is invaild: it is set to default value of 1000")
-        price=1000
-    return price*(1/(1-discount_rate/100))
