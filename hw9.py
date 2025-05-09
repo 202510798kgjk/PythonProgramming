f0f89cd528d92d597cc5373240e79cfb4aa8617c
202510798kgjk <202510798kgjk@gmail.com>
2025-05-09 15:02:34 +0900

    Update hw9.py

--- hw9.py
+++ hw9.py
@@ -1,1 +1,10 @@
-교수님 서버가 다운돼서 pdf 파일을 다운받지 못하였습니다. 나중에라도 올리겠습니다.
+#교수님 서버가 다운돼서 pdf 파일을 다운받지 못하였습니다. 지금 올렸습니다.
+basketList={}
+while True:
+    name=input("상품명? ")
+    if(name=="quit"):
+        break
+    num=input("수량은? ")
+
+    basketList[name]=num
+print("현재 장바구니: ", basketList)
